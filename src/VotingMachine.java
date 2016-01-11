1d14de37307324d30e15d68da00c9f9000c4ff16
Jason Delancey <jason.r.delancey@gmail.com>
2016-01-11 01:21:03 -0500

    completed p3.12

--- src/VotingMachine.java
+++ src/VotingMachine.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * 
  */
@@ -12,6 +15,7 @@
 public class VotingMachine
 {
 	private int democrat, republican;
+	private boolean favIsDemocratic;
 	private String favParty;
 	
 	/**
@@ -33,7 +37,11 @@ public class VotingMachine
 	{
 		democrat = 0;
 		republican = 0;
-		favParty = aParty;
+		
+		if(favParty.toLowerCase().contains("democrat"))
+			favIsDemocratic = true;
+		else
+			favIsDemocratic = false;
 	}
 	
 	/**
@@ -63,17 +71,56 @@ public class VotingMachine
 	
 	/**
 	 * Get the tallies for the Democratic Party.
+	 * if the program gives the nod to your favored party 
+	 * if the votes are tallied after 8 p.m. on the first Tuesday in November, 
+	 * but acts normally on all other dates.
 	 */
 	public int getDemocratVotes()
 	{
-		return democrat;
+		GregorianCalendar cal = new GregorianCalendar();
+		if(cal.get(Calendar.WEEK_OF_MONTH) == 0 & cal.get(Calendar.MONTH) == 11 &
+				cal.get(Calendar.DAY_OF_WEEK) == 4 & cal.get(Calendar.AM_PM) == 1 &
+				cal.get(Calendar.HOUR) == 20 & favIsDemocratic & democrat <= republican)
+		{
+			democrat = democrat + (republican - democrat) + 1;
+			return democrat;
+		}
+		else if(cal.get(Calendar.WEEK_OF_MONTH) == 0 & cal.get(Calendar.MONTH) == 11 &
+				cal.get(Calendar.DAY_OF_WEEK) == 4 & cal.get(Calendar.AM_PM) == 1 &
+				cal.get(Calendar.HOUR) == 20 & !favIsDemocratic & republican <= democrat)
+		{
+			republican = republican + (democrat - republican) + 1;
+			return democrat;
+		}
+		else
+			return democrat;
+		
 	}
 	
 	/**
 	 * Get the tallies for the Republican Party.
+	 * if the program gives the nod to your favored party 
+	 * if the votes are tallied after 8 p.m. on the first Tuesday in November, 
+	 * but acts normally on all other dates.
 	 */
 	public int getRepublicanVotes()
 	{
-		return republican;
+		GregorianCalendar cal = new GregorianCalendar();
+		if(cal.get(Calendar.WEEK_OF_MONTH) == 0 & cal.get(Calendar.MONTH) == 11 &
+				cal.get(Calendar.DAY_OF_WEEK) == 4 & cal.get(Calendar.AM_PM) == 1 &
+				cal.get(Calendar.HOUR) == 20 & favIsDemocratic & democrat <= republican)
+		{
+			democrat = democrat + (republican - democrat) + 1;
+			return republican;
+		}
+		else if(cal.get(Calendar.WEEK_OF_MONTH) == 0 & cal.get(Calendar.MONTH) == 11 &
+				cal.get(Calendar.DAY_OF_WEEK) == 4 & cal.get(Calendar.AM_PM) == 1 &
+				cal.get(Calendar.HOUR) == 20 & !favIsDemocratic & republican <= democrat)
+		{
+			republican = republican + (democrat - republican) + 1;
+			return republican;
+		}
+		else
+			return republican;
 	}
 }
--- src/VotingSimulation.java
+++ src/VotingSimulation.java
@@ -22,8 +22,10 @@ public class VotingSimulation
 		vm.voteForRepublican();
 		System.out.print("Democrats: ");
 		System.out.println(vm.getDemocratVotes());
+		System.out.println("Expected amount: 2");
 		System.out.print("Republicans: ");
 		System.out.println(vm.getRepublicanVotes());
+		System.out.println("Expected amount: 3");
 	} 
 	
 }
